bc8f39f6cd0749efcafb2668587fbc9468dfa863
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-03-30 15:39:06 +0200

    Add first structur of the classes

---
+++ ITLab/Models/Feedback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab.Models
+{
+    public class Feedback
+    {
+        public User Author { get; set; }
+        public String Content { get; set; }
+    }
+}
---
+++ ITLab/Models/Session.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab.Models
+{
+    public enum State
+    {
+        FINISHED, OPEN, CLOSED
+    }
+
+
+    public class Session
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Guestname { get; set; }
+        public Classroom Classroom { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public int  MaxAttendees { get; set; }
+        public IEnumerable<int> Media { get; set; }
+        public string VideoUrl { get; set; }
+        public IEnumerable<User> RegisteredUsers { get; set; }
+        public IEnumerable<User> Attendees { get; set; }
+        public User Host { get; set; }
+        public IEnumerable<Feedback> Feedbacks { get; set; }
+        protected State StateName { get; set; }
+        protected SessionState State { get; set; }
+
+    }
+}
