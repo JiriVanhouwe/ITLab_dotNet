ef745fb9e60d4d991370e63ae2f7f754c163f0a7
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-03-30 15:42:50 +0200

    Add campus and classroomcategory to the Classroom

--- ITLab/Models/Classroom.cs
+++ ITLab/Models/Classroom.cs
@@ -5,7 +5,27 @@ using System.Threading.Tasks;
 
 namespace ITLab.Models
 {
+    public enum Campus
+    {
+        GENT, AALST
+    }
+
+    public enum ClassRoomCategory
+    {
+        AUDITORIUM,
+        CLASSROOM,
+        PCCLASSROOM,
+        ITLAB,
+        MEETINGROOM,
+        LAPTOPCLASSROOM,
+        LANGUAGELAB
+    }
+
     public class Classroom
     {
+        public string Id { get; set; }
+        public Campus Campus { get; set; }
+        public int MaxSeats { get; set; }
+        public ClassRoomCategory RoomCategory { get; set; }
     }
 }
