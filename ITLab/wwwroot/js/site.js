0221344f27a615207ce618c9589e7aac25adaa41
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-04-25 12:02:10 +0200

    Add animation to nav bar, first version some fixes still needed

--- ITLab/wwwroot/js/site.js
+++ ITLab/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function initialize() {
+    
+}
+
+window.onscroll = changeNavigation;
+function changeNavigation() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navigation").classList.remove("emptyNav");
+        document.getElementById("navigation").classList.add("fullNav");
+    } else {
+        document.getElementById("navigation").classList.remove("fullNav");
+        document.getElementById("navigation").classList.add("emptyNav");
+    }
+}
+
+
+window.onload = initialize;
